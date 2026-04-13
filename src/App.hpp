5e02495b99851de5e98fc813a4f5336894341ed1
honorine <honorineru@gmail.com>
2026-04-13 17:22:34 +0200

    blue skybox

--- src/App.hpp
+++ src/App.hpp
@@ -1,33 +1,37 @@
 #pragma once
-#include "Menu/Menu.hpp"
-#include "Render2D/GameRender.hpp"
-#include "Game/Game.hpp"
-#include "Game/Utils.hpp"
 #include <memory>
 #include <optional>
-
-#include "Render3D/OpenGL.hpp" 
+#include "Game/Game.hpp"
+#include "Game/Utils.hpp"
+#include "Menu/Menu.hpp"
+#include "Render2D/GameRender.hpp"
 #include "Render3D/Board/Board.hpp"
+#include "Render3D/OpenGL.hpp"
 #include "Render3D/Shader.hpp"
+#include "Render3D/Skybox.hpp"
 
-enum class AppState { MainMenu, InGame };
+
+enum class AppState { MainMenu,
+                      InGame };
 
 class App {
 private:
-    AppState m_state = AppState::MainMenu;
-    std::unique_ptr<Game> m_current_game; 
-    
-    Menu m_menu;
+    AppState              m_state = AppState::MainMenu;
+    std::unique_ptr<Game> m_current_game;
+
+    Menu       m_menu;
     GameRender m_render2D;
 
+    Skybox                m_skybox;
+    std::optional<Shader> m_skyboxShader;
 
-    Board m_chessBoard3D;
+    Board                 m_chessBoard3D;
     std::optional<Shader> m_shader;
-    GLuint fbo = 0, textureColorbuffer = 0, rbo = 0;
-    int viewWidth = 800, viewHeight = 600;
+    GLuint                fbo = 0, textureColorbuffer = 0, rbo = 0;
+    int                   viewWidth = 800, viewHeight = 600;
 
 public:
-    void run(); 
-    void start_game(GameMode mode); 
+    void run();
+    void start_game(GameMode mode);
     void return_to_menu();
 };
---
+++ src/Shaders/skybox.fs
@@ -0,0 +1,5 @@
+#version 330 core
+out vec4 FragColor;
+void main() {
+    FragColor = vec4(0.53, 0.81, 0.92, 1.0); // Ton bleu ciel
+}
