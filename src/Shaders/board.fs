efb4b23b1a0ab6d5da6ce7a09d05629cced85473
honorine <honorineru@gmail.com>
2026-04-16 12:56:09 +0200

    clean shaders

--- src/Shaders/board.fs
+++ src/Shaders/board.fs
@@ -1,15 +1,14 @@
 #version 330 core
 out vec4 FragColor;
-
 in vec3 FragPos;
 in vec3 Normal;
 
-// Variables envoyées depuis App.cpp et Board.hpp
-uniform vec3 baseColor;
-uniform vec3 lightColor;     // La couleur de la chaine de Markov
-uniform vec3 lightPos;       // La lumière fixe en hauteur
-uniform vec3 mobileLightPos; // La lumière dirigée par tes boutons
-uniform int isWhiteTurn;     // 1 = Blanc, 0 = Noir
+// --- VARIABLES GLOBALES ---
+uniform int isWhiteTurn;  
+uniform vec3 baseColor;      
+uniform vec3 lightColor;     // Couleur Markov
+uniform vec3 lightPos;       
+uniform vec3 mobileLightPos; 
 
 void main()
 {
@@ -18,36 +17,37 @@ void main()
 
     if (isWhiteTurn == 1) 
     {
-        // 1. Lumière Fixe (Ambiance Jour)
+        // Lumière fixe claire
         vec3 lightDir1 = normalize(lightPos - FragPos);
         float diff1 = max(dot(norm, lightDir1), 0.0);
-        vec3 diffuse1 = diff1 * vec3(0.8, 0.8, 0.8); // Lumière blanche douce
+        vec3 diffuse1 = diff1 * vec3(0.8, 0.8, 0.8); 
 
-        // 2. Lumière Mobile (Tes boutons !)
+        // Lumière boutons
         vec3 lightDir2 = normalize(mobileLightPos - FragPos);
         float diff2 = max(dot(norm, lightDir2), 0.0);
         
-        // On calcule une "atténuation" pour qu'elle fasse l'effet d'une lampe qui éclaire une zone
+        // Calcul distance lumière
         float distance = length(mobileLightPos - FragPos);
         float attenuation = 1.0 / (1.0 + 0.09 * distance + 0.032 * (distance * distance));
         
-        // Lumière mobile avec une teinte chaude (Orange/Jaune)
+        // Lumière boutons atténuation
         vec3 diffuse2 = diff2 * vec3(1.0, 0.6, 0.2) * attenuation; 
 
-        // On mélange le tout
-        vec3 ambient = 0.3 * baseColor; // Beaucoup de lumière ambiante
+        // Forte luminosité ambiante
+        vec3 ambient = 0.3 * baseColor; 
         result = ambient + (diffuse1 + diffuse2) * baseColor;
     } 
     else 
     {
-
+        // Lumière fixe sombre
         vec3 lightDir = normalize(lightPos - FragPos);
         float diff = max(dot(norm, lightDir), 0.0);
         
-        // On utilise ta couleur de Markov pour colorer l'ambiance !
+        // Couleur Markov
         vec3 diffuse = diff * lightColor; 
 
-        vec3 ambient = 0.1 * baseColor; // Très peu de lumière ambiante (Nuit)
+        // Faible luminosité ambiante
+        vec3 ambient = 0.1 * baseColor; 
         result = ambient + diffuse * baseColor;
     }
 
--- src/Shaders/skybox.fs
+++ src/Shaders/skybox.fs
@@ -1,5 +1,6 @@
 #version 330 core
 out vec4 FragColor;
+
 void main() {
-    FragColor = vec4(0.53, 0.81, 0.92, 1.0); // Ton bleu ciel
+    FragColor = vec4(0.53, 0.81, 0.92, 1.0); //bleu
 }
